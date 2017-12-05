acb2fd58aae05de2e3502f8942324cc2aa244704
noemdb <noemdb@gmail.com>
2017-12-05 15:52:08 +0000

    implementado los usersetting

--- app/Models/sys/Setting.php
+++ app/Models/sys/Setting.php
@@ -7,6 +7,9 @@ use Cache;
 
 class Setting extends Model
 {
+
+	protected $fillable = ['name','value'];
+	
 	/*INI relaciones entre modelos*/
 	public function user()
     {
---
+++ app/Traits/UserSettingsTrait.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Traits;
+// use App\Models\sys\Setting;
+use App\Models\sys\Setting;
+use Illuminate\Support\Facades\Cache;
+
+trait UserSettingsTrait {
+
+    // get setting value
+    public function getSetting($name)
+    {
+        $settings = $this->getCache();
+        $value = array_get($settings, $name);
+        return ($value !== '') ? $value : NULL;
+    }
+
+    // create-update setting
+    public function setSetting($name, $value)
+    {
+        $this->storeSetting($name, $value);
+        $this->setCache();
+    }
+
+    // create-update multiple settings at once
+    public function setSettings($data = [])
+    {
+        foreach($data as $name => $value)
+        {
+            $this->storeSetting($name, $value);
+        }
+        $this->setCache();
+    }
+
+    private function storeSetting($name, $value)
+    {
+        $record = Setting::where(['user_id' => $this->id, 'name' => $name])->first();
+        if($record)
+        {
+            $record->value = $value;
+            $record->save();
+        } else {
+            //dd($name,$value);
+            $data = new Setting(['name' => $name, 'value' => $value]);
+            $this->settings()->save($data);
+        }
+    }
+
+    private function getCache()
+    {
+        if (Cache::has('user_settings_' . $this->id))
+        {
+            return Cache::get('user_settings_' . $this->id);
+        }
+        return $this->setCache();
+    }
+
+    private function setCache()
+    {
+        if (Cache::has('user_settings_' . $this->id))
+        {
+            Cache::forget('user_settings_' . $this->id);
+        }
+        $settings = $this->settings->pluck('value','name');
+        Cache::forever('user_settings_' . $this->id, $settings);
+        return $this->getCache();
+    }
+
+}
